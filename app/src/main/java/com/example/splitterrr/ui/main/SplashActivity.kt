db606b838b23584cff2dffe8a23129abb9556176
Dheeraj <dheeraj2@indiamart.com>
2025-03-29 16:16:00 +0530

    splash screen added

---
+++ app/src/main/java/com/example/splitterrr/ui/main/SplashActivity.kt
@@ -0,0 +1,28 @@
+package com.example.splitterrr.ui.main
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.splitterrr.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.splash_screen)
+
+        // Navigate to MainActivity after a short delay
+//        CoroutineScope(Dispatchers.Main).launch {
+//            delay(1500)
+//            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+//            finish() // Prevent back navigation to SplashActivity
+//        }
+
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+}
